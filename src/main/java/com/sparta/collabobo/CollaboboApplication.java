6be6323fe8487d76ca645018967edb089b49b4df
LeeJaeHyung <leejh7623@gmail.com>
2024-03-18 19:21:17 +0900

    first commit

---
+++ src/main/java/com/sparta/collabobo/CollaboboApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.collabobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboboApplication.class, args);
+	}
+
+}
