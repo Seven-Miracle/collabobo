d909fd1a9aec3e3e192a1478a46e55a3f8a66a04
si eun <tldms123455@naver.com>
2024-03-22 19:56:37 +0900

    :sparkles:  : feat : querydsl 추가

--- src/main/java/com/sparta/collabobo/card/repository/CardRepository.java
+++ src/main/java/com/sparta/collabobo/card/repository/CardRepository.java
@@ -3,6 +3,6 @@ package com.sparta.collabobo.card.repository;
 import com.sparta.collabobo.card.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository extends JpaRepository<Card, Long> {
+public interface CardRepository extends JpaRepository<Card, Long>, CardQuery {
 
 }
